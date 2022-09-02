e1c1517c80e8731ca505ab0dfa3cc085c43d3836
Adnan <adnannisat@gmail.com>
2022-09-03 04:44:22 +0600

    Style erase button

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const container = document.querySelector("#container");
 
-for(i=1;i<=16*16;i++){
+let gridSize = 16;
+
+
+for(i=1;i<=gridSize*gridSize;i++){
     const pixel = document.createElement('div');
     pixel.classList.add('pixel');
     pixel.setAttribute("id","pixel")
@@ -19,6 +22,25 @@ pixels.forEach((pixel) => {pixel.addEventListener("mouseleave",() => {
     pixel.classList.remove('hover');
 })});
 
-pixels.forEach((pixel) => {pixel.addEventListener("click",() => {
+let isDrawing = false; //isDrawing variable
+
+pixels.forEach((pixel) => {pixel.addEventListener("mousedown",() => {
     pixel.classList.add('onclick');  //Add color on click
+    isDrawing = true;
 })});
+
+pixels.forEach((pixel) => {pixel.addEventListener("mousemove",() => {
+    if(isDrawing) pixel.classList.add('onclick');  //Add color on drag
+})});
+
+pixels.forEach((pixel) => {pixel.addEventListener("mouseup",() => {
+    isDrawing = false;  //Stop drawing on mouse up
+})});
+
+const eraseButton = document.getElementById("eraseButton");
+eraseButton.addEventListener("click",() => {
+    pixels.forEach((pixel) => {
+        if(pixel.classList.contains('onclick'))  //erase 
+        pixel.classList.remove('onclick');
+    });
+});
